bc58b64a52952ce3386d465a43e73c10f0966693
YigitDilibal <archery10@outlook.com>
2025-04-11 14:44:47 +0300

    appium day04

--- src/test/java/pages/AllCurrencyPage.java
+++ src/test/java/pages/AllCurrencyPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import tests.utilities.Driver;
 
@@ -9,5 +11,23 @@ public class AllCurrencyPage {
     public AllCurrencyPage(){
         PageFactory.initElements((WebDriver) Driver.getAndroidDriver(),this);
     }
+
+
+    @FindBy(xpath = "//*[@text='CURRENCY CONVERTER']" )
+    public WebElement uygulamaAdi;
+
+    @FindBy(xpath = "//*[@text='2']" )
+    public WebElement IkiTusu;
+
+    @FindBy(xpath = "//*[@text='00']" )
+    public WebElement IkiSifirTusu;
+
+    @FindBy(id = "com.smartwho.SmartAllCurrencyConverter:id/EditTextCurrencyB")
+    public WebElement sonuc;
+
+
+
+
+
 
 }
--- src/test/java/tests/day04/AllCurrency.java
+++ src/test/java/tests/day04/AllCurrency.java
@@ -2,28 +2,61 @@ package tests.day04;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AllCurrencyPage;
 import tests.utilities.Driver;
+import tests.utilities.ReusableMethods;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AllCurrency {
-
     AndroidDriver<AndroidElement> driver= Driver.getAndroidDriver();
-
-    AllCurrencyPage page = new AllCurrencyPage();
+    AllCurrencyPage page=new AllCurrencyPage();
 
     @Test
-    public void test01(){
-
+    public void AllCurrencyConverterTest() throws InterruptedException, IOException {
         // AllCurrency Converter uygulamasinin yuklendigi dogulanir
+        Assert.assertTrue(driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter"));
+
         // Uygulamanin acildigi dogrulanir
-        // Cevirmek istedigimiz para birimiKanada dolari olarak secilir
+        Assert.assertTrue(page.uygulamaAdi.isDisplayed());
+        //Assert.assertTrue(driver.findElementByXPath("//*[@text='CURRENCY CONVERTER']").isDisplayed());
+
+        // Cevirmek istedigimiz para birimi Kanada dolari olarak secilir
+        ReusableMethods.koordinatTiklamaMethodu(350,450,500);
+        Thread.sleep(2000);
+        ReusableMethods.scrollWithUiScrollableAndClick("Canadian Dollar");
+
         // Cevirilecek tutar tuslanir
+        page.IkiTusu.click();
+        Thread.sleep(2000);
+        page.IkiSifirTusu.click();
+
+
         // Cevirilecek olan para birimi Türk lirasi olarak secilir
+        ReusableMethods.koordinatTiklamaMethodu(350,650,500);
+        Thread.sleep(2000);
+        ReusableMethods.scrollWithUiScrollableAndClick("TRY");
+
         // Cevirilen tutar screenshot olarak kaydedilir
+        ReusableMethods.getScreenshot("ParaCeviriSonucu");
+
+        File screenshot=driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot,new File("screenshot.jpg"));
+
         // Ardindan Kanada dolarinin Türk lirasi karsiligi olan degeri kaydedilir
+        String sonucDegeri =page.sonuc.getText();
+
         // Sonuc kullaniciya sms olarak bildirilir
+        driver.sendSMS("55","200 Kanada Dolarinin Türk lirasi karsiligi: "+sonucDegeri);
 
     }
+
+
+
 
 }
